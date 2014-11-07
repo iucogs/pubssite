425784f369292c54e0e3c5f910d2c82d0c78f827
Patrick Craig <pjcraig@indiana.edu>
2014-11-07 13:01:37 -0500

    current progress on update/auth/parser

--- pubssite/views.py
+++ pubssite/views.py
@@ -12,21 +12,30 @@ from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from sqlalchemy.sql import exists
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy import update
+from sqlalchemy import and_
 from .models import *
 
+# Dev note: flushes at the beginning of get views are for consistency between
+# developer versions.
 
 # parser setup
 ctx = rython.RubyContext(requires=["rubygems", "anystyle/parser"])
+ctx("Encoding.default_internal = 'UTF-8'")
+ctx("Encoding.default_external = 'UTF-8'")
 parser = ctx("Anystyle.parser")
 
 log = logging.getLogger(__name__)
 
 ## SITE VIEWS ##
 
+# for stuff that probably doesn't exist. cushions the blow by returning some U2
+# lyrics.
 @notfound_view_config(append_slash=True)
 def notfound(request):
     return HTTPNotFound('404: and I stiiiiill haven\'t found what I\'m looking for...')
 
+# if you were wondering where the beef is, it's here.
+# this returns the site.
 @view_config(route_name='home', renderer='templates/index.mako')
 def home(request):
     log.debug('home')
@@ -38,7 +47,9 @@ def home(request):
     #return HTTPFound(request.route_path('login'))
     return {'ok': 'ok'}
 
-
+# authenticates a user. or probably will at some point in the future, we hope.
+# INPUT: a request object containing the CAS ticket
+# OUTPUT: authenticated user credential object including permission
 # TODO: ret auth user obj
 @view_config(route_name='login', renderer='templates/login.mako')
 def login(request):
@@ -64,12 +75,14 @@ def login(request):
             log.debug(remember(request, pubs_username))
             log.debug('rldl')
             headers = remember(request, pubs_username)
-            log.debug(headers)
+           # log.debug(headers)
             return HTTPFound(request.route_path('home'), headers=headers)
 
     else:
         return {'ok': 'log in now!'}
 
+# INPUT: a request object with user credentials
+# OUTPUT: returns user to the homepage with cleared user credentials
 def logout(request):
     headers = forget(request)
     loc = request.route_url('home')
@@ -77,10 +90,18 @@ def logout(request):
 
 ## CITATION API VIEWS ##
 
+@view_config(route_name='citation_add', request_method='POST', renderer='pubs_json')
+def citation_add(request):
+    citation = str(request.body)
+    citation = parser.parse(citation)
+    return citation
+
+# this one updates a citation and the authors associated with it.
+# INPUT: request object containing a JSON encoded citation in the JSON body
+# OUTPUT: the citation's updated JSON
 #TODO: add permission user
 @view_config(route_name='citation_update', request_method='PUT', renderer='pubs_json')
 def citation_update(request):
-    log.debug('triggered')
     new_citation = request.json_body
     new_cit_authors = new_citation['authors'] 
     # following try/catch clean up the citation dict for update, making the
@@ -94,17 +115,58 @@ def citation_update(request):
         del new_citation['auth_string']
     except KeyError:
         pass
-
+    
+    # update citation fields like title, year, etc.
     Session.execute(update(Citation).where(Citation.citation_id == new_citation['citation_id']).values(new_citation))
     
+    # list of tuples of the citation's current author list containing their
+    # author_ids
+    current_authors = [auth[0] for auth in Session.query(author_of).filter(author_of.c.citation_id == new_citation['citation_id']).all()]
+    
+    # iterate over the new authors to see if we're updating or adding a new
+    # author
     for author in new_cit_authors:
-        Session.execute(update(Author).where(Author.author_id == author['author_id']).values(author))
+        author_exists = Session.query(Author).filter(and_(Author.lastname.like(author['lastname']), Author.firstname.like(author['firstname']))).all()                
+        if author_exists:
+            log.debug("author exists: ")
+            log.debug(author_exists)
+            log.debug(author['lastname'] + ' exists')
+            if author['author_id'] in current_authors:
+                pass
+            else:
+                Session.execute(update(author_of).where(author_of.c.citation_id == new_citation['citation_id'] and 
+                author_of.c.author_id == author['author_id']).values(citation_id=new_citation['citation_id'], 
+                author_id=author['author_id']))
+                              
+        else:
+            new_author = Author(author['firstname'], author['lastname'])
+            #new_author = Author('Donny', 'Belasko')
+            log.debug("This is the new author.")
+            log.debug(author)
+            Session.add(new_author)
+            Session.commit()
+            new_author_id = Session.query(Author).filter(Author.lastname == author['lastname'] and Author.firstname == author['firstname'])
+            
+            Session.execute(update(author_of).where(author_of.c.citation_id == new_citation['citation_id'] and 
+            author_of.c.author_id == author['author_id']).values(citation_id = new_citation['citation_id'], author_id = new_author_id))
+    
+    # Removes authors no longer part of the citation
+    for current_author in current_authors:
+        if current_author not in [author['author_id'] for author in new_cit_authors]:
+            Session.execute(remove(author_of).where(author_of.c.citation_id == new_citation['citation_id'] and 
+            author_of.c.author_id == current_author))
     
     Session.commit()
+    log.debug(new_cit_authors)
     updated_cit = Session.query(Citation).get(new_citation['citation_id'])
-    
     return updated_cit.json
 
+# deletes a citation?????????????
+# INPUT: A request object containing the citation id of the citation to be
+# deleted
+# OUTPUT: A response indicating the success of the operation
+# TODO: match user with citation owner so that only owners can delete
+# citations to prevent total academic anarchy
 @view_config(route_name='citation_delete', request_method='DELETE',
              permission='user')
 def delete_citation(request):
@@ -117,11 +179,18 @@ def delete_citation(request):
     else:
         return HTTPNotFound('citation not found')
 
+# returns a single citation object or a list of citation objects
+# as determined by function args
+# INPUT: A request object containing a single citation ID or a list of IDs 
+# delimited by commas
+# OUPUT: the JSON of the corresponding ID(s)
 @view_config(route_name='citation_by_id', renderer='pubs_json')
 def citation_by_id(request):
     id = str(request.matchdict.get('id', -1))
     citations = []
-
+    
+    # here we check for multiple citation_ids, separated by commas
+    # if it's one citation, break by returning it. otherwise, the else.
     if "," not in id:
         citation = Session.query(Citation).get(id)
         if not citation:
@@ -138,6 +207,11 @@ def citation_by_id(request):
                 citations.append(citation.json) 
         return citations
 
+# takes a string, the owner, and returns all citations associated with this
+# owner.
+# INPUT: a string representing the owner of a citation
+# OUTPUT: A JSON array containing the JSON of all citations associated with the
+# owner
 @view_config(route_name='citations_by_owner', renderer='pubs_json')
 def citations_by_owner(request):
     owner = str(request.matchdict.get('owner', -1))
@@ -148,6 +222,9 @@ def citations_by_owner(request):
         return HTTPNotFound()
     return [citation.json for citation in citations]
 
+# retuns a json object containing the citations in a collection given by the id
+# INPUT: A request object containing a collection ID
+# OUTPUT: a JSON array of citation JSON objects
 @view_config(route_name='citations_by_collection', renderer='pubs_json')
 def citations_by_collection(request):
     id = int(request.matchdict.get('id', -1))
@@ -159,6 +236,10 @@ def citations_by_collection(request):
 
 ## COLLECTION API VIEWS ##
 
+# this seems to delete a collection. very dangerous.
+# INPUT: request object containing the ID of the collection to be deleted
+# OUTPUT: A message relaying the success of the operation
+# TODO: make sure the user can only delete their own collections
 @view_config(route_name='collection_delete', request_method='DELETE',
              permission='user')
 def delete_collection(request):
@@ -171,6 +252,10 @@ def delete_collection(request):
     else:
         return HTTPNotFound('collection not found')
 
+# returns a collection object given by the input id
+# INPUT: request object containing the ID of the collection to be retrieved 
+# OUTPUT: the collection object
+# TODO: allow support for retrieving multiple collection objects at once
 @view_config(route_name='collection_by_id', renderer='pubs_json')
 def collection_by_id(request):
     id = int(request.matchdict.get('id', -1))
@@ -180,6 +265,9 @@ def collection_by_id(request):
         return HTTPNotFound()
     return collection.json
 
+# returns a list of collection objects association with an owner
+# INPUT: request object containing the owner's name
+# OUTPUT: a JSON array of the JSON objects representing the owner's collections
 @view_config(route_name='collections_by_owner', renderer='pubs_json')
 def collections_by_owner(request):
     owner = str(request.matchdict.get('owner', -1))
