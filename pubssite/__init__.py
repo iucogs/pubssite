0a0ceac5964cb4335e7e08fdcf6afc7eba1db19a
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-05-10 15:50:01 -0400

    adding mako dependecy injection to Configurator

--- pubssite/__init__.py
+++ pubssite/__init__.py
@@ -27,6 +27,7 @@ def main(global_config, **settings):
     authn_policy = AuthTktAuthenticationPolicy('iu_cas')
     authz_policy = ACLAuthorizationPolicy()
     config = Configurator(settings=settings)
+    config.include('pyramid_mako')
 
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
